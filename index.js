96caa01f036b32b0de51c380e6f2187d4f366828
tomyrd <trodriguez@fi.uba.ar>
2021-12-08 15:30:04 -0300

    Middleware

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const pool = new Pool({
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(express.json());
 
 
